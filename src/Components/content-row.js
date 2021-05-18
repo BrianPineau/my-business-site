489d672d06b7bbf8cc60a7837a04c207bb815a91
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2021-05-17 23:00:05 -0500

    Hover effect added to card

--- src/Components/content-row.js
+++ src/Components/content-row.js
@@ -1,15 +1,28 @@
+import React, { useState } from 'react';
 
 import './component-css/content-row.css';
 
 const ContentRow = () => {
+
+  let [className, animateCard] = useState(false);
+  
+  console.log(animateCard);
   return (
     <div>
       <div className="section-container">
         <div className="flex-container">
           <section className="card card-1">
-            <div className="card-image"></div>
+            <div
+              onMouseEnter = {() => animateCard(true)}
+              onMouseLeave = {() => animateCard(false)}
+              className="card-image">
+              <div className={className === false ? "hover-card-1" : "hover-card-1-show" }>POW!</div>
+            </div>
 
-            <div className="card-title"></div>
+            <div className="card-info">
+              <h1 className="card-title">BUY ME PLEASE</h1>
+              <p className="card-text">Ratatat tat cat in a hat</p>
+            </div>
           </section>
 
 
