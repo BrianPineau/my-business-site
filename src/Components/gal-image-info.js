8b0e75594e6076db7f1d118260ea6d1514316b28
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2021-07-01 17:12:58 -0500

    margins added to image info section

--- src/Components/gal-image-info.js
+++ src/Components/gal-image-info.js
@@ -7,8 +7,17 @@ const GalleryImageInfo = () => {
 
   return (
     <div>
-      <div className="image-info-container">SUUUUUUUURGE!!!</div>
-      <div className="image-section"></div>
+      <div className="image-info-container">
+
+        <div className="image-container">
+          <div className="big-image"></div>
+        </div>
+
+        <div className="side-bar-container">
+          <div className="similar-images"></div>
+        </div>
+
+      </div>
     </div>
   )
 }
