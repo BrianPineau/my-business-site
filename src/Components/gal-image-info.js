bd52237468826ab8cf9d6a2a42ff4042de1cc010
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2021-09-16 17:34:46 -0500

    added magnify image on gal-image-info component

--- src/Components/gal-image-info.js
+++ src/Components/gal-image-info.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 
 import SmallHeader from './small-header';
 import FullImageCard from './full-image-card';
@@ -7,6 +7,15 @@ import './component-css/gal-image-info.css';
 
 const GalleryImageInfo = () => {
 
+  let [chooseClass, popOutWindow] = useState(false);
+
+  // const toggleWindow = () => {
+  //   chooseClass ? popOutWindow(true) : popOutWindow(false);
+  //   console.log(chooseClass);
+  // }
+  const closeWindow = () => {
+    chooseClass ? popOutWindow(false) : popOutWindow(true);
+  }
  
   return (
     <div>
@@ -14,7 +23,11 @@ const GalleryImageInfo = () => {
       <div className="image-info-container">
 
         <div className="image-container">
-          <div href="" className="big-image"></div>
+          <div onClick={() => popOutWindow()} className="big-image"></div>
+        </div>
+
+        <div onClick={() => closeWindow()} className={chooseClass === false ? "full-screen-img-off" : "full-screen-img-on"}>
+          <div  className="full-screen-image"></div>
         </div>
 
         <div className="side-bar-container">
