3376518ab0db842712ffcf857f22ee36ba6659f6
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2022-07-04 16:10:08 -0500

    changed header to side header

--- src/Components/header.js
+++ src/Components/header.js
@@ -26,33 +26,66 @@ const Header = () => {
 
   return (
     <div>
-      <header className={scrolled === false ? "full-header": "full-header-shrink"}>
-        <div className="header-container">
+      <header className="vert-header">
+        <div className="header-container vheader-container">
 
-          <div className="top-row">
-            <div className="social-media">media</div>
-            <div className="logo"><div className="logo-image"></div></div>
-            <div className="tools">tools</div>
+          <div className="top-row vtop-row">
+            
+            <div className="logo vlogo"><div className="logo-image"></div></div>
+            {/* <div className="tools">tools</div> */}
           </div>
 
-          <div className="bottom-row">
+          <div className="bottom-row vert-bottom-row">
 
-            <div className={scrolled === false ? "social-media-pretrigger": "social-media-triggered"}>media</div> 
+            {/* <div className={scrolled === false ? "social-media-pretrigger": "social-media-triggered"}>media</div>  */}
 
-            <Link style={noMoStylin} to="./"><div className="home">HOME</div></Link>
-            <div className="shop">SHOP</div>
-            <Link style={noMoStylin} to="./gallery"><div className="gallery">GALLERY</div></Link>
-            <div className="about">ABOUT</div>
-            <div className="contact">CONTACT</div>
-            <div className="sales">SALES</div>
+            <Link style={noMoStylin} to="./"><div className="home vhome">HOME</div></Link>
+            <Link><div className="shop vshop">SHOP</div></Link>
+            <Link style={noMoStylin} to="./gallery"><div className="gallery vgallery">GALLERY</div></Link>
+            <Link><div className="about vabout">ABOUT</div></Link>
+            <Link><div className="contact vcontact">CONTACT</div></Link>
+            <Link><div className="sales vsales">SALES</div></Link>
 
-            <div className={scrolled === false ? "tools-pretrigger": "tools-triggered"}>tools</div>
+            {/* <div className={scrolled === false ? "tools-pretrigger": "tools-triggered"}>tools</div> */}
 
           </div>
 
+          <div className="social-media">media</div>
         </div>
       </header>
     </div>
+
+
+    
+    
+    // <div>
+    //   <header className={scrolled === false ? "full-header": "full-header-shrink"}>
+    //     <div className="header-container">
+
+    //       <div className="top-row">
+    //         <div className="social-media">media</div>
+    //         <div className="logo"><div className="logo-image"></div></div>
+    //         <div className="tools">tools</div>
+    //       </div>
+
+    //       <div className="bottom-row">
+
+    //         <div className={scrolled === false ? "social-media-pretrigger": "social-media-triggered"}>media</div> 
+
+    //         <Link style={noMoStylin} to="./"><div className="home">HOME</div></Link>
+    //         <div className="shop">SHOP</div>
+    //         <Link style={noMoStylin} to="./gallery"><div className="gallery">GALLERY</div></Link>
+    //         <div className="about">ABOUT</div>
+    //         <div className="contact">CONTACT</div>
+    //         <div className="sales">SALES</div>
+
+    //         <div className={scrolled === false ? "tools-pretrigger": "tools-triggered"}>tools</div>
+
+    //       </div>
+
+    //     </div>
+    //   </header>
+    // </div>
   )
 }
 
